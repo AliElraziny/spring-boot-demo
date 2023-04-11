b2cfe17ba5f44f6d45a1a40a6bb0da1888756e3b
aly elraziny <alyelraziny@gmail.com>
2023-04-11 22:37:42 +0200

    first commit

---
+++ src/test/java/com/try2code/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.try2code.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
